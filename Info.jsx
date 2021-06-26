726c8e9c87c60d91cf120d3cce15236f4312f0a7
Jitendra Kushwah <jitendrakushwahsingh@gmail.com>
2021-06-26 16:36:12 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info(){
+  return (
+    <div className="note">
+      <h1>we dev</h1>
+      <p>slekgnkfndgl</p>
+    </div>
+  );
+}
+
+export default Info;

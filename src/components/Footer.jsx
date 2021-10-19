36e2eb58b9404a7a5121b268599d46cef1c2576b
Jitendra <jitendrakushwahsingh@gmail.com>
2021-10-19 19:20:32 +0530

    final

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer(){
   return (
     <footer>
-      <p>Copyright by ShapeAI @ {new Date().getFullYear()}</p>
+      <p>Copyright by JSK @ {new Date().getFullYear()}</p>
     </footer>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Header(){
   return (
     <header>
-      <h1>ShapeAI</h1>
+      <h1>JSK</h1>
     </header>
   );
 }
